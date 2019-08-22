34a65741d808d351fdb42d87152a84137b32df01
geospoc <geospoc@localhost.localdomain>
2019-08-22 11:31:27 +0000

    deploy8

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$(/home/ubuntu/.local/bin/aws ecr get-login --no-include-email)
+$(aws ecr get-login --no-include-email)
 docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest
