db917bd371823a432f5b2bb099e376164752e707
geospoc <geospoc@localhost.localdomain>
2019-08-23 11:11:23 +0000

    deploy18

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email)
-docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest
+docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
