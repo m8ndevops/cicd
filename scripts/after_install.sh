dd323396348717ba82c4aa4d7c63ae11e56907ba
geospoc <geospoc@localhost.localdomain>
2019-08-23 06:33:48 +0000

    deploy14

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -d -p 80:80 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest
+
+isPresent=$(docker container ls -f name=nginx | awk '{print $1}' | tail -n +2)
+if [ "$isPresent" != "" ];then
+    docker rm -f nginx >> /home/ubuntu/commits
+    docker run -d -p 80:80 --name nginx 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
+else
+    docker run -d -p 80:80 --name nginx nginx >> /home/ubuntu/commits
+fi
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-users
 $(aws ecr get-login --no-include-email)
 docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest
