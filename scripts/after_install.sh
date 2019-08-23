bd899d6618db6e461eefdc110d684e63b36f36f9
geospoc <geospoc@localhost.localdomain>
2019-08-23 06:39:47 +0000

    deploy15

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,5 +5,5 @@ if [ "$isPresent" != "" ];then
     docker rm -f nginx >> /home/ubuntu/commits
     docker run -d -p 80:80 --name nginx 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
 else
-    docker run -d -p 80:80 --name nginx nginx >> /home/ubuntu/commits
+    docker run -d -p 80:80 --name nginx 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
 fi
