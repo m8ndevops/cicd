9cb22ea3550dc6b109f36d4b83fb08601318d07e
geospoc <geospoc@localhost.localdomain>
2019-08-26 15:58:57 +0530

    docker compose test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+$(aws ecr get-login --no-include-email)
+docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
+
 isPresent=$(docker container ls -f name=nginx | awk '{print $1}' | tail -n +2)
 if [ "$isPresent" != "" ];then
     docker rm -f nginx >> /home/ubuntu/commits
