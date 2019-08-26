12213224f90948d2da3545c7e84599e154ce204b
geospoc <geospoc@localhost.localdomain>
2019-08-26 19:24:30 +0530

    CD V1

--- docker.sh
+++ docker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email)
-echo "This is the PATH script running"
-cd /home/ubuntu/
-pwd
-ls
+docker pull 173437313385.dkr.ecr.ap-south-1.amazonaws.com/test:latest >> /home/ubuntu/commits
 docker-compose up -d
